13d7377fe6a3d115535085c306f480229e5ae803
zslim <zsofia.limbek@gmail.com>
2018-08-22 09:46:29 +0200

    developed basic counter

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+counts = 0
 
 
+@app.route("/")
+def main_page():
+    global counts
+    return render_template("index.html", counter=counts)
+
+
+@app.route("/request-counter")
+def request_counter():
+    global counts
+    counts += 1
+    return redirect("/")
 
 
 if __name__ == "__main__":
